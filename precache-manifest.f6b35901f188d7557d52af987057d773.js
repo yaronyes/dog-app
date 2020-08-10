5a06f182d177badedfe64c2c02dbe3efa90c35b9
yaronyes <yaronyes@hotmail.com>
2020-08-10 23:01:08 +0300

    Updates

---
+++ precache-manifest.f6b35901f188d7557d52af987057d773.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39c3135e1ac5dcca3775b02aad59f53b",
+    "url": "/dog-app/index.html"
+  },
+  {
+    "revision": "68891587fdd0163d7c87",
+    "url": "/dog-app/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "0638a8bbd1005faa5e28",
+    "url": "/dog-app/static/css/main.5c175ad5.chunk.css"
+  },
+  {
+    "revision": "68891587fdd0163d7c87",
+    "url": "/dog-app/static/js/2.24a8a5e4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dog-app/static/js/2.24a8a5e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0638a8bbd1005faa5e28",
+    "url": "/dog-app/static/js/main.6fc284b2.chunk.js"
+  },
+  {
+    "revision": "cbf6ae1a5e15235d6422",
+    "url": "/dog-app/static/js/runtime-main.eece9791.js"
+  }
+]);
