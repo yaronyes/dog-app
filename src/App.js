06af4a4cee2e726ccc8776bc9e92b24be4cd4164
yaronyes <yaronyes@hotmail.com>
2020-08-10 10:38:02 +0300

    adding routing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import HeaderNav from './components/HeaderNav'
+import { HashRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   const [hideNavbar, setHideNavbar] = useState(false);
 
   return (
     <div className="App">
-      <HeaderNav hideNavbar={hideNavbar}/>
+      <HashRouter>
+        <HeaderNav hideNavbar={hideNavbar}/>
+        <Switch>
+          <Route exact path="/">
+
+          </Route>
+          <Route exact path="/breeds">
+
+          </Route>
+          <Route exact path="/breeds/:breedName">
+
+          </Route>
+        </Switch>
+      </HashRouter>
+      
     </div>
   );
 }
